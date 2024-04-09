1e786bf511b41b368bc68f56bb9b78f7cae28920
Danny Castilla <dancastiv@gmail.com>
2024-04-09 11:50:54 +0100

    Initial Commit

---
+++ YourProject/YourProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourProjectApp.swift
+//  YourProject
+//
+//  Created by Daniela Castilla on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct YourProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
